75781de87eed3c37e6e885fa399b037b4f6b4f36
feMoraes0 <fernandomoraes.lopes@gmail.com>
2020-07-20 21:51:43 -0300

    tab icons and name removed

--- src/App.js
+++ src/App.js
@@ -4,15 +4,58 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Flower from './tabs/Flower';
 import None from './tabs/None';
+import FlowerIcon from '../assets/icons/flower.svg';
+import HeartIcon from '../assets/icons/heart.svg';
+import UserIcon from '../assets/icons/user.svg';
 
 const App = () => {
   const {Navigator, Screen} = createBottomTabNavigator();
 
   return (
     <NavigationContainer>
-      <Navigator>
-        <Screen name="flowers" component={Flower} />
-        <Screen name="favourites" component={None} />
+      <Navigator
+        tabBarOptions={{
+          showLabel: false,
+        }}>
+        <Screen
+          name="flowers"
+          component={Flower}
+          options={{
+            tabBarIcon: ({focused}) => (
+              <FlowerIcon
+                width={30}
+                heigth={30}
+                fill={focused ? '#3C4046' : '#979797'}
+              />
+            ),
+          }}
+        />
+        <Screen
+          name="favourites"
+          component={None}
+          options={{
+            tabBarIcon: ({focused, inactiveTintColor, activeTintColor}) => (
+              <HeartIcon
+                width={30}
+                heigth={30}
+                fill={focused ? '#3C4046' : '#979797'}
+              />
+            ),
+          }}
+        />
+        <Screen
+          name="person"
+          component={None}
+          options={{
+            tabBarIcon: ({focused}) => (
+              <UserIcon
+                width={30}
+                heigth={30}
+                fill={focused ? '#3C4046' : '#979797'}
+              />
+            ),
+          }}
+        />
       </Navigator>
     </NavigationContainer>
   );
--- src/tabs/None.js
+++ src/tabs/None.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import FlowerIcon from '../../assets/icons/flower.svg';
 
 const None = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Not defined</Text>
+      <FlowerIcon width={30} heigth={30} />
     </View>
   );
 };
