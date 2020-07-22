30368922c3c4ec19d4a6d4bc682c0878d3dfbba1
feMoraes0 <fernandomoraes.lopes@gmail.com>
2020-07-22 16:35:40 -0300

    added animation with button control

--- src/components/NavigationDrawer/index.js
+++ src/components/NavigationDrawer/index.js
@@ -1,16 +1,46 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useRef, useState} from 'react';
+import {Animated, View, Text, TouchableOpacity} from 'react-native';
 import styles from './style.js';
 
 const NavigationDrawer = ({name, children}) => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const heightAnimate = useRef(new Animated.Value(260)).current;
+
+  const openMenu = () => {
+    Animated.timing(heightAnimate, {
+      toValue: 0,
+      timing: 500,
+      useNativeDriver: true,
+    }).start();
+    setMenuOpen(true);
+  };
+
+  const closeMenu = () => {
+    Animated.timing(heightAnimate, {
+      toValue: 260,
+      timing: 500,
+      useNativeDriver: true,
+    }).start();
+    setMenuOpen(false);
+  };
+
   return (
-    <View style={styles.navigationDrawer}>
-      <View style={styles.buttonTop} />
+    <Animated.View
+      style={[
+        styles.navigationDrawer,
+        {transform: [{translateY: heightAnimate}]},
+      ]}>
+      <TouchableOpacity
+        onPress={() => {
+          menuOpen ? closeMenu() : openMenu();
+        }}>
+        <View style={styles.buttonTop} />
+      </TouchableOpacity>
       <Text style={styles.title}>{name}</Text>
       <Text style={styles.subtitle}>Tulip • 37 days • Healthy</Text>
       <View style={styles.divider} />
       {children}
-    </View>
+    </Animated.View>
   );
 };
 
