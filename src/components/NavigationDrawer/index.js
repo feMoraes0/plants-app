c4559a3aacb47255e0532ff6b30c28126abedea9
feMoraes0 <fernandomoraes.lopes@gmail.com>
2020-07-22 17:26:45 -0300

    added animation with gesture control

--- src/components/NavigationDrawer/index.js
+++ src/components/NavigationDrawer/index.js
@@ -1,46 +1,82 @@
-import React, {useRef, useState} from 'react';
-import {Animated, View, Text, TouchableOpacity} from 'react-native';
+import React from 'react';
+import {Animated, View, Text} from 'react-native';
 import styles from './style.js';
+import {PanGestureHandler, State} from 'react-native-gesture-handler';
 
 const NavigationDrawer = ({name, children}) => {
-  const [menuOpen, setMenuOpen] = useState(false);
-  const heightAnimate = useRef(new Animated.Value(260)).current;
+  let offset = 0;
+  const heightAnimate = new Animated.Value(0);
+  const animatedEvent = Animated.event(
+    [
+      {
+        nativeEvent: {
+          translationY: heightAnimate,
+        },
+      },
+    ],
+    {useNativeDriver: true},
+  );
 
-  const openMenu = () => {
-    Animated.timing(heightAnimate, {
-      toValue: 0,
-      timing: 500,
-      useNativeDriver: true,
-    }).start();
-    setMenuOpen(true);
-  };
+  const handleMenu = (event) => {
+    if (event.nativeEvent.oldState === State.ACTIVE) {
+      let isOpen = true;
+      const {translationY} = event.nativeEvent;
+      offset += translationY;
 
-  const closeMenu = () => {
-    Animated.timing(heightAnimate, {
-      toValue: 260,
-      timing: 500,
-      useNativeDriver: true,
-    }).start();
-    setMenuOpen(false);
+      if (translationY >= 10) {
+        isOpen = false;
+      } else {
+        heightAnimate.setValue(offset);
+        heightAnimate.setOffset(0);
+        offset = 0;
+      }
+
+      Animated.timing(heightAnimate, {
+        toValue: isOpen ? 0 : 260,
+        timing: 1000,
+        useNativeDriver: true,
+      }).start(() => {
+        offset = isOpen ? 0 : 260;
+        heightAnimate.setOffset(offset);
+        heightAnimate.setValue(0);
+      });
+    }
   };
 
   return (
-    <Animated.View
-      style={[
-        styles.navigationDrawer,
-        {transform: [{translateY: heightAnimate}]},
-      ]}>
-      <TouchableOpacity
-        onPress={() => {
-          menuOpen ? closeMenu() : openMenu();
-        }}>
+    <PanGestureHandler
+      onGestureEvent={animatedEvent}
+      onHandlerStateChange={handleMenu}>
+      <Animated.View
+        style={[
+          styles.navigationDrawer,
+          {
+            transform: [
+              {
+                translateY: heightAnimate.interpolate({
+                  inputRange: [0, 260],
+                  outputRange: [0, 260],
+                  extrapolate: 'clamp',
+                }),
+              },
+            ],
+          },
+        ]}>
         <View style={styles.buttonTop} />
-      </TouchableOpacity>
-      <Text style={styles.title}>{name}</Text>
-      <Text style={styles.subtitle}>Tulip • 37 days • Healthy</Text>
-      <View style={styles.divider} />
-      {children}
-    </Animated.View>
+        <Text style={styles.title}>{name}</Text>
+        <Text style={styles.subtitle}>Tulip • 37 days • Healthy</Text>
+        <View style={styles.divider} />
+        <Animated.View
+          style={{
+            opacity: heightAnimate.interpolate({
+              inputRange: [0, 220],
+              outputRange: [1, 0],
+            }),
+          }}>
+          {children}
+        </Animated.View>
+      </Animated.View>
+    </PanGestureHandler>
   );
 };
 
