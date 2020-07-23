6e3564afd27766d97e9a86447ea456c5d70fe992
feMoraes0 <fernandomoraes.lopes@gmail.com>
2020-07-23 16:31:32 -0300

    added blur radius animated teorically correct but it doesn't work, react-native issue opened

--- src/components/NavigationDrawer/index.js
+++ src/components/NavigationDrawer/index.js
@@ -1,48 +1,15 @@
 import React from 'react';
 import {Animated, View, Text} from 'react-native';
 import styles from './style.js';
-import {PanGestureHandler, State} from 'react-native-gesture-handler';
+import {PanGestureHandler} from 'react-native-gesture-handler';
 
-const NavigationDrawer = ({name, children}) => {
-  let offset = 0;
-  const heightAnimate = new Animated.Value(0);
-  const animatedEvent = Animated.event(
-    [
-      {
-        nativeEvent: {
-          translationY: heightAnimate,
-        },
-      },
-    ],
-    {useNativeDriver: true},
-  );
-
-  const handleMenu = (event) => {
-    if (event.nativeEvent.oldState === State.ACTIVE) {
-      let isOpen = true;
-      const {translationY} = event.nativeEvent;
-      offset += translationY;
-
-      if (translationY >= 10) {
-        isOpen = false;
-      } else {
-        heightAnimate.setValue(offset);
-        heightAnimate.setOffset(0);
-        offset = 0;
-      }
-
-      Animated.timing(heightAnimate, {
-        toValue: isOpen ? 0 : 260,
-        timing: 1000,
-        useNativeDriver: true,
-      }).start(() => {
-        offset = isOpen ? 0 : 260;
-        heightAnimate.setOffset(offset);
-        heightAnimate.setValue(0);
-      });
-    }
-  };
-
+const NavigationDrawer = ({
+  animatedEvent,
+  handleMenu,
+  heightAnimate,
+  name,
+  children,
+}) => {
   return (
     <PanGestureHandler
       onGestureEvent={animatedEvent}
--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -1,16 +1,64 @@
 import React from 'react';
-import {SafeAreaView, View, Image, TouchableOpacity} from 'react-native';
+import {Animated, SafeAreaView, View, TouchableOpacity} from 'react-native';
 import styles from './style.js';
 import ArrowLeft from '../../../assets/icons/arrow-left.svg';
 import Dots from '../../../assets/icons/dots.svg';
 import NavigationDrawer from '../../components/NavigationDrawer';
 import SliderCustom from '../../components/SliderCustom';
+import {State} from 'react-native-gesture-handler';
 
 const Details = ({route, navigation}) => {
   const {flower} = route.params;
+  let offset = 0;
+  const heightAnimate = new Animated.Value(0);
+  const animatedEvent = Animated.event(
+    [
+      {
+        nativeEvent: {
+          translationY: heightAnimate,
+        },
+      },
+    ],
+    {useNativeDriver: true},
+  );
+
+  const handleMenu = (event) => {
+    if (event.nativeEvent.oldState === State.ACTIVE) {
+      let isOpen = true;
+      const {translationY} = event.nativeEvent;
+      offset += translationY;
+
+      if (translationY >= 10) {
+        isOpen = false;
+      } else {
+        heightAnimate.setValue(offset);
+        heightAnimate.setOffset(0);
+        offset = 0;
+      }
+
+      Animated.timing(heightAnimate, {
+        toValue: isOpen ? 0 : 260,
+        timing: 1000,
+        useNativeDriver: true,
+      }).start(() => {
+        offset = isOpen ? 0 : 260;
+        heightAnimate.setOffset(offset);
+        heightAnimate.setValue(0);
+      });
+    }
+  };
+
   return (
-    <>
-      <Image style={styles.image} source={flower.image} />
+    <View>
+      <Animated.Image
+        style={styles.image}
+        source={flower.image}
+        blurRadius={heightAnimate.interpolate({
+          inputRange: [0, 200],
+          outputRange: [0, 1],
+          extrapolate: 'clamp',
+        })}
+      />
       <SafeAreaView style={styles.frontPage}>
         <View style={styles.header}>
           <TouchableOpacity onPress={() => navigation.pop()}>
@@ -19,7 +67,11 @@ const Details = ({route, navigation}) => {
           <Dots />
         </View>
       </SafeAreaView>
-      <NavigationDrawer name={flower.name}>
+      <NavigationDrawer
+        animatedEvent={animatedEvent}
+        handleMenu={handleMenu}
+        heightAnimate={heightAnimate}
+        name={flower.name}>
         <SliderCustom
           minimumValue={1}
           maximumValue={6}
@@ -45,7 +97,7 @@ const Details = ({route, navigation}) => {
           label="CO2 PUMP"
         />
       </NavigationDrawer>
-    </>
+    </View>
   );
 };
 
